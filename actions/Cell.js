332183dca16847c3b6d2ca94951e0098fd7aa596
Boris Filipov <obiwonn@gmail.com>
2013-01-15 16:23:57 +0200

    added cwd support for Cell start

--- actions/Cell.js
+++ actions/Cell.js
@@ -71,7 +71,7 @@ module.exports = organic.Organel.extend(function Cell(plasma, config){
       var cmd = ". ~/.nvm/nvm.sh; nvm use "+process.version+"; "+
         "cd "+path.dirname(c.target)+"; git pull; "+
         "npm install; "+
-        "angel Tissue start "+c.target;
+        "angel Tissue start "+c.target+" "+c.cwd;
       var child = shelljs.exec("ssh "+c.remote+' "'+cmd+'"', {async: true});
       child.on('exit', function(code){
         if(callback) callback({data: code});
@@ -80,7 +80,8 @@ module.exports = organic.Organel.extend(function Cell(plasma, config){
       this.emit({
         type: "Tissue",
         action: "start",
-        target: c.target
+        target: c.target,
+        cwd: c.cwd
       }, callback);
     }
   }
